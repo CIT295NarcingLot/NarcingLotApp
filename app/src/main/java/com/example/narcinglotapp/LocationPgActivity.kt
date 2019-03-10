21c4394d21e13067e4b2f6e927826e0a1a72c8c7
NDeardoff <46725249+NDeardoff@users.noreply.github.com>
2019-03-10 18:43:31 -0500

    Adds Next and Previous Page functionality

--- app/src/main/java/com/example/narcinglotapp/LocationPgActivity.kt
+++ app/src/main/java/com/example/narcinglotapp/LocationPgActivity.kt
@@ -1,2 +1,27 @@
 package com.example.narcinglotapp
 
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.location_pg.*
+
+// Creates the Location instance and is required for an activity
+class LocationPgActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.location_pg)
+
+        // Sends user to Submitter Page
+        nextButton.setOnClickListener{
+            val intent = Intent(this, SubmitterPgActivity :: class.java)
+            startActivity(intent)
+        }
+
+        // Sends user back to Offender Page
+        preButton.setOnClickListener{
+            val intent = Intent(this, OffenderPgActivity :: class.java)
+            startActivity(intent)
+        }
+
+    }
+}
